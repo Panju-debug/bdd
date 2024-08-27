8889e6c3074ab4be93646dbddb0da4e0c5c94fcd
Panju-debug <pablovizuete13@gmail.com>
2024-08-27 15:46:20 -0500

    reto 5

---
+++ actualizaciones/creacion_cuentas.sql
@@ -0,0 +1,53 @@
+create table cuentas(
+	numero_cuenta char(5) not null,
+	cedula_propietario char(5) not null,
+	fecha_creacion date not null,
+	saldo money not null,
+	constraint cuentas_pk primary key (numero_cuenta)
+)
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12345','98765','20/06/2024',100)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12346','98766','21/06/2024',200)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12347','98768','22/06/2024',300)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12348','98769','23/06/2024',400)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12349','98760','24/06/2024',500)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12340','98761','25/06/2024',600)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12341','98762','26/06/2024',700)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12342','98763','27/06/2024',800)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12343','98764','28/06/2024',900)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12244','17723','29/06/2024',1000)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12144','17223','29/06/2024',1000)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values ('12044','17323','29/06/2024',1000)
+
+select numero_cuenta,saldo from cuentas 
+
+select * from cuentas
+where fecha_creacion between'27/06/2024'and '27/08/2024'
+
+select numero_cuenta,saldo from cuentas
+where fecha_creacion between '27/06/2024' and '27/08/2024'
+
+update cuentas set saldo=10
+where cedula_propietario like '17%'
---
+++ actualizaciones/creacion_transacciones.sql
@@ -0,0 +1,57 @@
+create table transacciones(
+	codigo int,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hora time not null,
+	constraint transacciones_pk primary key (codigo)
+)
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (1,'12345',100.00,'C','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (11,'12445',200.00,'D','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (12,'12345',300.00,'C','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (4,'12345',10.00,'D','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5,'12345',10.00,'C','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (13,'12345',500.00,'D','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (7,'12345',10.00,'C','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (14,'12345',700.00,'D','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (9,'12345',10.00,'C','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (10,'12345',10.00,'D','27/08/2024','11:30')
+
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (23,'12345',499.00,'D','27/09/2024','14:30')
+
+
+
+select * from transacciones 
+where tipo ='D'
+
+select *from transacciones
+where monto between money(200) and money(2000)
+
+select codigo,monto,tipo,fecha from transacciones
+where fecha <> null
+
+update transacciones set tipo='T'
+where monto between money(101) and money(499) and
+fecha between '01/09/2024' and '30/09/2024' and
+hora between '14:00' and '20:00'

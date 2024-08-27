e537c7ed268513674128b145eae3ee0ecf975a0c
Panju-debug <pablovizuete13@gmail.com>
2024-08-27 09:14:08 -0500

    reto 1

---
+++ creacion_estudiantes.sql
@@ -0,0 +1,7 @@
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	aoellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null
+)
---
+++ creacion_videojuegos.sql
@@ -0,0 +1,6 @@
+create table videojuegos(
+	codigo int not null,
+	nombre varchar(100) not null,
+	descripcion varchar(100),
+	valoracion int not null
+)

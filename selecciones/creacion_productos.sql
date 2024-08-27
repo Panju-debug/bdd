e31fa7ed53c53f87f49c9840a3eafdd47f007c42
Panju-debug <pablovizuete13@gmail.com>
2024-08-27 14:49:45 -0500

    reto 4

---
+++ selecciones/creacion_productos.sql
@@ -0,0 +1,43 @@
+create table productos(
+	codigo int not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	precio money not null,
+	stock int not null,
+	constraint productos_pk primary key (codigo)
+)
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('123','Jabon','Buena calidad',1.50,'10')
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('124','Papas','Buena calidad',3.00,'11')
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('125','Detergente','Buena calidad',5.00,'10')
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('126','Bebida','Buena calidad',1.00,'15')
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('127','Pan','Buena calidad',0.25,'30')
+
+insert into productos(codigo,nombre,precio,stock)
+values ('1234','Aceite',1.50,'30')
+
+insert into productos(codigo,nombre,precio,stock)
+values ('1235','Fideos',1.50,'30')
+
+insert into productos(codigo,nombre,precio,stock)
+values ('1236','Mortadela',1.50,'30')
+
+insert into productos(codigo,nombre,precio,stock)
+values ('1237','Queso',3.00,'15')
+
+select * from productos 
+where nombre like 'Q%'
+
+select * from productos
+where descripcion is null
+
+select *from productos
+where precio between money(2) and money(3)
---
+++ selecciones/creacion_registros.sql
@@ -0,0 +1,40 @@
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null
+)
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('123','1754506450','27/08/2024','9:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('124','1754506451','27/08/2024','8:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('125','1754506452','27/08/2024','17:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('126','1754506453','27/08/2024','13:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('127','1754506455','27/08/2024','17:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('128','1754506456','27/08/2024','17:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('129','1754506457','27/08/2024','17:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('120','1754506458','27/08/2024','17:30')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values ('121','1754506459','27/08/2024','17:30')
+
+select cedula_empleado,fecha,hora from registros_entrada
+
+select *from registros_entrada
+where hora between '7:00' and '14:00'
+
+select * from registros_entrada 
+where hora>'8:00'

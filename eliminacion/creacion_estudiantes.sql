01cf780ad36fa4377d2bd3b1c977c66c2628ac27
Panju-debug <pablovizuete13@gmail.com>
2024-08-29 07:28:09 -0500

    Reto 6

---
+++ eliminacion/creacion_estudiantes.sql
@@ -0,0 +1,45 @@
+
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null
+)
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567890','Pablo','Vizuete','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567891','Mario','Neta','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567892','Armando','Paredes','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567893','Alan','Brito','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567894','Alma','Madero','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1234567895','Elpo','Jonh','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1734567896','Jorge','Nitales','hola@gmail.com','27/08/2024')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1734567897','Elsa','Pato','hola@gmail.com','27/08/2024')
+
+select nombre,cedula from estudiantes
+
+select * from estudiantes
+where cedula like '17%'
+
+select nombre,apellido from estudiantes
+where nombre like 'A%'
+
+update estudiantes set apellido='Hernandez'
+where cedula like '17%'
+
+delete from estudiantes
+where cedula like '%05'
---
+++ eliminacion/creacion_videojuegos.sql
@@ -0,0 +1,44 @@
+create table videojuegos(
+	codigo int not null,
+	nombre varchar(100) not null,
+	descripcion varchar(100),
+	valoracion int not null
+)
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (123,'Residen Evil','chido',10)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (124,'Pokemon','chido',10)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (125,'Fifa','chido',8)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (126,'Michi Money','chido',9)
+
+insert into videojuegos (codigo,nombre,descripcion,valoracion)
+values (127,'Cario 64','chido',7)
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (111,'Mario 63',5)
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (112,'Mario 62',10)
+
+insert into videojuegos (codigo,nombre,valoracion)
+values (113,'Mario 61',9)
+
+select * from videojuegos
+where nombre like 'C%'
+
+select * from videojuegos
+where valoracion between 9 and 10
+
+select * from videojuegos
+where descripcion is null
+
+update videojuegos set descripcion='Mejor apuntado'
+where valoracion>9
+
+delete from videojuegos 
+where valoracion<7
